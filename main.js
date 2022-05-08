e20b8aec6f2618a0d2cf83bfdd220b84006349ea
Emily  Phee <emilyphee@Emilys-MacBook-Air.local>
2022-05-08 16:23:22 -0700

    Handle create a new board  with input grid size

--- main.js
+++ main.js
@@ -1,21 +1,65 @@
-const container = document.querySelector('.container');
-const resetBtn = document.querySelector('.reset-btn');
+const container = document.querySelector('.grid-container');
+const resetBtn = document.querySelector('.clear-btn');
+const gridCountInput = document.querySelector('#grid-count');
+const submitBtn = document.querySelector('.submit-btn');
+
+let gridSize = 16;
+
+createGrid();
 
 resetBtn.addEventListener('click', () => removeClass());
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.classList.add('block');
-  container.appendChild(div);
+submitBtn.addEventListener('click', () => {
+  gridSize = parseInt(gridCountInput.value);
+  console.log(gridSize);
+
+  // remove child nodes
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  createGrid();
+});
+
+console.log(gridSize);
+
+// for (let i = 0; i < gridSize * gridSize; i++) {
+//   const div = document.createElement('div');
+//   div.classList.add('block');
+//   // div.style.cssText = 'border: 1px solid black; height: 15px; width: 15px;';
+//   div.style.width = 'calc(500px / gridSize);';
+//   div.style.height = 'calc(500px / gridSize);';
+//   container.appendChild(div);
+// }
+
+// const blocks = document.querySelectorAll('.block');
+
+// // add class name "active" every hovered block
+// for (let block of blocks) {
+//   block.addEventListener('mouseenter', () => block.classList.add('active'));
+// }
+
+function createGrid() {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('block');
+    div.style.width = 'calc(500px / gridSize);';
+    div.style.height = 'calc(500px / gridSize);';
+    container.appendChild(div);
+  }
+
+  const blocks = document.querySelectorAll('.block');
+
+  // add class name "active" every hovered block
+  for (let block of blocks) {
+    block.addEventListener('mouseenter', () => block.classList.add('active'));
+  }
+
+  container.style.setProperty(
+    'grid-template-columns',
+    `repeat(${gridSize}, ${500 / gridSize}px [col-start])`
+  );
 }
-
-const blocks = document.querySelectorAll('.block');
-
-// add class name "active" every hovered block
-for (let block of blocks) {
-  block.addEventListener('mouseenter', () => block.classList.add('active'));
-}
-
 // remove all class name "active" from the list
 function removeClass() {
   blocks.forEach(block => {
