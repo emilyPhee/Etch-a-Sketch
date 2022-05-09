4e8423d3f931cc2541512a9d6b17183b84739608
Emily  Phee <emilyphee@Emilys-MacBook-Air.local>
2022-05-08 22:41:31 -0700

    Add input box and check valid user input for grid size

--- main.js
+++ main.js
@@ -10,8 +10,18 @@ createGrid();
 resetBtn.addEventListener('click', () => removeClass());
 
 submitBtn.addEventListener('click', () => {
+  if (
+    parseInt(gridCountInput.value) > 100 ||
+    parseInt(gridCountInput.value) < 5 ||
+    parseInt(gridCountInput.value) === NaN
+  ) {
+    alert('Please choose grid size between 5 to 100');
+
+    // remove text input
+    gridCountInput.value = '';
+  }
+
   gridSize = parseInt(gridCountInput.value);
-  console.log(gridSize);
 
   // remove child nodes
   while (container.firstChild) {
@@ -19,25 +29,10 @@ submitBtn.addEventListener('click', () => {
   }
 
   createGrid();
+
+  // remove text input
+  gridCountInput.value = '';
 });
-
-console.log(gridSize);
-
-// for (let i = 0; i < gridSize * gridSize; i++) {
-//   const div = document.createElement('div');
-//   div.classList.add('block');
-//   // div.style.cssText = 'border: 1px solid black; height: 15px; width: 15px;';
-//   div.style.width = 'calc(500px / gridSize);';
-//   div.style.height = 'calc(500px / gridSize);';
-//   container.appendChild(div);
-// }
-
-// const blocks = document.querySelectorAll('.block');
-
-// // add class name "active" every hovered block
-// for (let block of blocks) {
-//   block.addEventListener('mouseenter', () => block.classList.add('active'));
-// }
 
 function createGrid() {
   for (let i = 0; i < gridSize * gridSize; i++) {
@@ -60,8 +55,10 @@ function createGrid() {
     `repeat(${gridSize}, ${500 / gridSize}px [col-start])`
   );
 }
+
 // remove all class name "active" from the list
 function removeClass() {
+  const blocks = document.querySelectorAll('.block');
   blocks.forEach(block => {
     block.classList.remove('active');
   });
